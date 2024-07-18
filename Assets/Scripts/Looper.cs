faa0e3b0f9f4b77bc8b97b4596b70fcaae82069c
Jake Gresh <85658632+SuperstarJFG@users.noreply.github.com>
2024-07-17 21:28:15 -0700

    fix children being teleported on loop

--- Assets/Scripts/Looper.cs
+++ Assets/Scripts/Looper.cs
@@ -8,6 +8,7 @@ public class Looper : MonoBehaviour
     private float verticalBound = 5f;
     private float horizontalBound = 5f * 16f/9f;
     private List<Vector3> childPositions = new List<Vector3>();
+    private Vector3 newPosition;
     void Start()
     {
         
@@ -17,14 +18,18 @@ public class Looper : MonoBehaviour
     {
         foreach (Transform child in transform)
             childPositions.Add(child.position);
+
+        newPosition = transform.position;
         if (transform.position.y > verticalBound)
-            transform.position = new Vector3(transform.position.x, -verticalBound + 0.01f, transform.position.z);
+            newPosition.y = -verticalBound + 0.01f;
         if (transform.position.y < -verticalBound)
-            transform.position = new Vector3(transform.position.x, verticalBound - 0.01f, transform.position.z);
+            newPosition.y = verticalBound - 0.01f;
         if (transform.position.x > horizontalBound)
-            transform.position = new Vector3(-horizontalBound + 0.01f, transform.position.y, transform.position.z);
+            newPosition.x = -horizontalBound + 0.01f;
         if (transform.position.x < -horizontalBound)
-            transform.position = new Vector3(horizontalBound - 0.01f, transform.position.y, transform.position.z);
+            newPosition.x = horizontalBound - 0.01f;
+        transform.position = newPosition;
+
         foreach (Transform child in transform)
             child.position = childPositions[0];
         childPositions.Clear();
